98d58d4e1ad5f7ea4eac8e79df8348a69ce6dbcc
dkavalerov <kavalerov.d@yandex.ru>
2019-08-05 16:28:12 +0300

    Разбираюсь с ООП: set() / get(), Ключевое слово static. Статические поля и методы.

---
+++ OOP/src/com/oop/Main.java
@@ -0,0 +1,37 @@
+package com.oop;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Dog's count "+Dog.getDogsCount());
+
+        Dog lab = new Dog();
+        lab.setPaws(4);
+        lab.setTail(1);
+        lab.setName("Charley");
+        lab.setBreed("Lab");
+        lab.setSize("Average");
+        lab.bite();
+
+        Dog sheppard = new Dog();
+        sheppard.setPaws(4);
+        sheppard.setTail(1);
+        sheppard.setName("Mike");
+        sheppard.setBreed("Sheppard");
+        sheppard.setSize("Big");
+        sheppard.bite();
+
+
+        Dog doberman = new Dog();
+        doberman.setPaws(4);
+        doberman.setTail(1);
+        doberman.setName("Leonard");
+        doberman.setBreed("Doberman");
+        doberman.bite();
+
+
+//        System.out.println("Lab's name is "+ lab.getName());
+//        System.out.println("Sheppard's name is "+ sheppard.getName());
+
+    }
+}
